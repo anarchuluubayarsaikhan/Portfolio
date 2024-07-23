196d1a0fca0bb76ec266b5b4267130e18d74b779
anarchuluubayarsaikhan <anarchulu@gmail.com>
2024-07-23 09:50:22 +0800

    Profile background adding

--- pages/components/profile.js
+++ pages/components/profile.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export function Profile() {
     return (
-        <div className=" m-auto w-full xl:max-w-[280px] xl:max-h-[320px]">
-            <Image src="/Pic.jpg" width={240} height={280} className="xl:order-2 w-full xl:max-w-[280px] xl:max-h-[320px]"/>
-        </div>
+            <div className=" m-auto w-full xl:aspect-[5/6] bg-gray-200 xl:max-w-[280px] xl:max-h-[320px] aspect-square">
+                <Image src="/Pic.jpg" width={240} height={280} className="xl:order-2 xl:w-full h-full block relative xl:bottom-8 xl:right-8 xl:left-[-32px] xl:object-cover xl:max-w-[280px] xl:max-h-[320px] w-5/6 left-[8%] bottom-10"/>
+            </div>
     )
 }
