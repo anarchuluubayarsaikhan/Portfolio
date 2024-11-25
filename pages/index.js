824ad0b3b8c47d1c560bbecbfb30354a462ea3de
Anarchuluu Bayarsaikhan <anarchulu@Anarchuluus-MacBook-Air.local>
2024-11-25 17:07:33 +0800

    main

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,22 @@ import { Education } from "@/components/education";
 import { Scroll } from "@/components/scroll";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000); 
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <main>
+      {isLoading ? (
+        <div className="flex justify-center items-center h-screen">
+          <div className="w-8 h-8 border-8 border-t-transparent border-black border-solid rounded-full animate-spin"></div>
+        </div>
+      ) : (
         <div className="lg:m-auto w-full">
           <Header />
           <Intro />
@@ -19,6 +33,7 @@ export default function Home() {
           <Contact />
           <Scroll />
         </div>
+      )}
     </main>
   );
 }
