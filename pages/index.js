a5bad51564536b3907395e9b2e63a5d34fe3c558
Anarchuluu Bayarsaikhan <anarchulu@Anarchuluus-MacBook-Air.local>
2024-11-25 13:15:02 +0800

    third

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,8 @@ export default function Home() {
       {/* <Header /> */}
       <Intro/>
        <Tech/>
-      {/* <Works/>
-      <Education/>
+      <Works/>
+      {/* <Education/>
       <Contact/>
       <Scroll/>  */}
       </div>
