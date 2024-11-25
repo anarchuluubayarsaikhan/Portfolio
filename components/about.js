5617e79bf28454573549903dd3100b2d39fe7d82
Anarchuluu Bayarsaikhan <anarchulu@Anarchuluus-MacBook-Air.local>
2024-11-25 12:43:18 +0800

    trying to solve margin

--- components/about.js
+++ components/about.js
@@ -1,7 +1,7 @@
 import TypingEffect from 'react-typing-effect'
 export function About(){
     return(
-        <div className="text-white flex gap-3 text-2xl lg:text-xl font-bold hover:cursor-pointer max-w-[400px] flex-wrap text-center">
+        <div className="text-white flex gap-3 text-2xl lg:text-xl font-bold hover:cursor-pointer lg:max-w-[400px] flex-wrap text-center">
         <TypingEffect
           text={["Сайн байна уу. Анарчулуу байна. Mиний портфолио хуудсанд тавтай морил ✌️"]}
           speed={100} 
